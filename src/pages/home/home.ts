744de1a4b508b467afcdfffb45b5bce5815fa836
solodynamo <bittuf3@gmail.com>
2016-10-20 18:01:21 +0530

    Home component created with rx get

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
 
+import {GithubService} from '../../app/services/github'
+
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [GithubService]
 })
 export class HomePage {
+  public foundRepos;
+  public username;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor (private github: GithubService) {
+
   }
+
+
+
+  getRepos() {
+
+    this.github.getRepos(this.username).subscribe(
+      
+      data => {
+        this.foundRepos =  data.json();
+      },
+
+      err => {
+         console.log("error occured", err);
+      },
+
+      () => {
+        console.log("i got the repos successfully");
+        
+      }
+    );
+
+  }
+
+
 
 }
